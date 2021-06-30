7a783b024dd4aa649f46f38ab44777df1fc650b0
josealfredojimenezgarcia <joseajimenez07@gmail.com>
2021-06-29 22:31:49 -0500

    Codigo Python

---
+++ index.py
@@ -0,0 +1,20 @@
+print("Dios es Amor");
+
+a=20;
+b=3;
+c=0;
+
+Conjunto={1,2,3,4,5,6,7,8,9};
+
+Lista=['valerie', 'Josesito', 'Stiven'];
+
+Tupla=(1,2,3,5,4,6,69,98,852,124);
+
+diccionario={a:'jose', b:'Stiven', c:'Valerie'};
+
+c=a**b;
+print(c)
+print("Valerie es Hermosa");
+h=str(input('Digite su nombre   '))
+print('Ustes se registró como: ', h)
+print('Gracias, Fin del Programa')
